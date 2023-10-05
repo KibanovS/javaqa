81e59c9688cc95f8f4c6da8a65ac20c2b85b7607
Slava Kibanov <kibanchous@mail.ru>
2023-10-05 11:58:21 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 13676; // стоимость билета
+
+        int bonus = 20; // количество рублей для начисления одной бонусной мили
+
+        System.out.println("Вам начисленно " + (amount / bonus) + " бонусных миль");
+
+    }
+}
